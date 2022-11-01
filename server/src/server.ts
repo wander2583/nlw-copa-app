6282434dde9ffa96dd32d8972a5e2e0a6466d2ed
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-11-01 09:19:05 -0300

    chore: configurando o DER

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,18 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   })
-
-  // http://localhost:3333/polls/
   
-  fastify.get('/pools/count', () => {
-    return { count: 123123123 }
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
+    return { count }
   })
 
   await fastify.listen({ port: 3333 })
