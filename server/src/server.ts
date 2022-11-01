4883d2ca1a9ddec6e7dcd8960788223d7702271e
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-11-01 08:13:04 -0300

    chore: instalando e configurando o Prisma/Client

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,12 @@ async function bootstrap() {
     logger: true,
   })
 
+  // http://localhost:3333/polls/
+  
+  fastify.get('/pools/count', () => {
+    return { count: 123123123 }
+  })
+
   await fastify.listen({ port: 3333 })
 }
 
